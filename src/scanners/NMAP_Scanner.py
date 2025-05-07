5d923a433c0202c2fd326380dfd7a35bad27e8c7
gmturn <112572411+gmturn@users.noreply.github.com>
2025-05-07 03:15:45 -0500

    updated config loading

--- src/scanners/NMAP_Scanner.py
+++ src/scanners/NMAP_Scanner.py
@@ -6,6 +6,6 @@ import os
 class NmapScanner:
     def __init__(self, config):
         self.config = config
-        self
+
         self.Scanner = nmap.PortScanner()
         self.ScanResults = {}
--- src/utilities/utils.py
+++ src/utilities/utils.py
@@ -9,7 +9,7 @@ def return_config(config_path):
 
         # Ensure Proper Data Types
         conf = {'IPRange': str(config['DEFAULT']['IPRange']), 'HostIP': str(config['DEFAULT']
-                                                                            ['HostIP']), 'HostMAC': str(config['DEFAULT']['HostMAC']), 'Timeout': int(config['DEFAULT']['Timeout']), 'Verbose': bool(config['DEFAULT']['Verbose']), 'ScanType': str(config['DEFAULT']['ScanType']), 'd_Data': str(config['DEFAULT']['d_Data'])}
+                                                                            ['HostIP']), 'HostMAC': str(config['DEFAULT']['HostMAC']), 'Timeout': int(config['DEFAULT']['Timeout']), 'Verbose': bool(config['DEFAULT']['Verbose']), 'ScanType': str(config['DEFAULT']['ScanType']), 'd_Data': str(config['DEFAULT']['d_Data']), 'ActiveIPs': str(config['DEFAULT']['ActiveIPs']), 'InactiveIPs': str(config['DEFAULT']['InactiveIPs']), 'NmapScanResults': str(config['DEFAULT']['NmapScanResults'])}
         return conf
     except:
         raise ValueError(f"Error: Could not load config file '{config_path}'")

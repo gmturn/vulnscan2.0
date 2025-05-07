41a826af697b62946c64803164e4357cd7b956ba
gmturn <112572411+gmturn@users.noreply.github.com>
2025-05-07 02:13:32 -0500

    added try and except to utils.py

--- src/utilities/utils.py
+++ src/utilities/utils.py
@@ -2,11 +2,14 @@ import configparser
 
 
 def return_config(config_path):
-    config = configparser.ConfigParser()
-    # config.read(config_path + "config.conf")
-    config.read(config_path)
 
-    # Ensure Proper Data Types
-    conf = {'IPRange': str(config['DEFAULT']['IPRange']), 'HostIP': str(config['DEFAULT']
-                                                                        ['HostIP']), 'HostMAC': str(config['DEFAULT']['HostMAC']), 'Timeout': int(config['DEFAULT']['Timeout']), 'Verbose': bool(config['DEFAULT']['Verbose']), 'ScanType': str(config['DEFAULT']['ScanType']), 'Data': str(config['DEFAULT']['Data'])}
-    return conf
+    try:
+        config = configparser.ConfigParser()
+        config.read(config_path)
+
+        # Ensure Proper Data Types
+        conf = {'IPRange': str(config['DEFAULT']['IPRange']), 'HostIP': str(config['DEFAULT']
+                                                                            ['HostIP']), 'HostMAC': str(config['DEFAULT']['HostMAC']), 'Timeout': int(config['DEFAULT']['Timeout']), 'Verbose': bool(config['DEFAULT']['Verbose']), 'ScanType': str(config['DEFAULT']['ScanType']), 'Data': str(config['DEFAULT']['Data'])}
+        return conf
+    except:
+        raise ValueError(f"Error: Could not load config file '{config_path}'")

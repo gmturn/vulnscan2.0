4612a73b50bf14cea0284e5fe2dc9bc71b3890e9
gmturn <112572411+gmturn@users.noreply.github.com>
2025-05-09 00:31:06 -0500

    removed unnecessary import

--- src/core/logger.py
+++ src/core/logger.py
@@ -6,7 +6,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..', '..', 'src')))
 
 from models.m_Device import NetworkDevice  # noqa: E402
-from utilities.write_to_file import *  # noqa: E402
 
 
 class Logger:
